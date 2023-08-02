6d964e8996b505952c97d033cdf84eb40f097c9c
Saad Amjad <saad.amjad@monstar-lab.com>
2023-08-02 12:43:24 +0600

    feature: updated pluginId

--- packages/core/upload/admin/src/pluginId.js
+++ packages/core/upload/admin/src/pluginId.js
@@ -1,5 +1,8 @@
-import pluginPkg from '../../package.json';
+// import pluginPkg from '../../package.json';
 
-const pluginId = pluginPkg.name.replace(/^@strapi\/plugin-/i, '');
+// Replacing this with 'upload' so that custom motf plugin can be referenced as upload package
+// const pluginId = pluginPkg.name.replace(/^@strapi\/plugin-/i, '');
+
+const pluginId = 'upload';
 
 export default pluginId;
